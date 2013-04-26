de29d63f86763a2d5fa8e60be8e6ec1e9d6284fd
Sergey Prischak <sergey.sntu@gmail.com>
2013-04-26 14:59:08 +0300

    add mail config to env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,9 @@ Invitask::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.action_mailer.default_url_options = configatron.emails.default_url_options.host
+
+  config.action_mailer.smtp_settings = configatron.emails.smtp_settings 
+
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,4 +34,9 @@ Invitask::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  config.action_mailer.default_url_options = configatron.emails.default_url_options.host
+
+  config.action_mailer.smtp_settings = configatron.emails.smtp_settings 
+  
 end
